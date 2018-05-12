302a07a594dc6ddb493cbbd3bfae1131f7e831e1
Pranav <pranav.dilip.patel@gmail.com>
2018-05-12 18:38:32 +0200

    room constructor made

--- SEP/src/model/Room.java
+++ SEP/src/model/Room.java
@@ -7,5 +7,16 @@ public class Room {
 	private boolean smokingAllowed;
 	private boolean availability;
 	
+	public Room() {
+		
+	}
+	
+	public Room(int roomNumber, int quality, int floor, boolean smokingAllowed, boolean availability) {
+		this.roomNumber = roomNumber;
+		this.quality = quality;
+		this.floor = floor;
+		this.smokingAllowed = smokingAllowed;
+		this.availability = availability;
+	}
 
 }
