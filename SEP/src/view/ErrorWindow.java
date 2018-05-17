65d3bbd26042397c24a759f8d95857ba0296f2aa
Pranav <pranav.dilip.patel@gmail.com>
2018-05-17 16:03:02 +0200

    Error window

---
+++ SEP/src/view/ErrorWindow.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class ErrorWindow {
+	
+	public void nameError() {
+		Alert nameAlert = new Alert(AlertType.ERROR);
+		nameAlert.setTitle("Error!");
+		nameAlert.setHeaderText("An Error has occured");
+		nameAlert.setContentText("Name can only contain letters!");
+		nameAlert.showAndWait();
+	}
+
+}
