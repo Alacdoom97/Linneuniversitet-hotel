9b692bbd3a490b2474186cfbb464137220923600
Pranav <pranav.dilip.patel@gmail.com>
2018-05-20 16:50:09 +0200

    RoomList filled with rooms

--- SEP/src/model/Room.java
+++ SEP/src/model/Room.java
@@ -7,18 +7,72 @@ public class Room {
 	private int floor;
 	private boolean smokingAllowed;
 	private boolean availability;
+	private boolean adjoinsRoom;
 	
 	public Room() {
 		
 	}
 	
-	public Room(String hotelName, int roomNumber, int quality, int floor, boolean smokingAllowed, boolean availability) {
+	public Room(String hotelName, int roomNumber, int quality, int floor, boolean smokingAllowed, boolean availability, boolean adjoinsRoom) {
 		this.hotelName = hotelName;
 		this.roomNumber = roomNumber;
 		this.quality = quality;
 		this.floor = floor;
 		this.smokingAllowed = smokingAllowed;
 		this.availability = availability;
+		this.adjoinsRoom = adjoinsRoom;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public int getQuality() {
+		return quality;
+	}
+
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+
+	public int getFloor() {
+		return floor;
+	}
+
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	public boolean isSmokingAllowed() {
+		return smokingAllowed;
+	}
+
+	public void setSmokingAllowed(boolean smokingAllowed) {
+		this.smokingAllowed = smokingAllowed;
+	}
+
+	public boolean isAvailability() {
+		return availability;
+	}
+
+	public void setAvailability(boolean availability) {
+		this.availability = availability;
+	}
+	
+	public boolean setAdjoinsRoom(boolean setAdjoins) {
+		return setAdjoins;
 	}
 
 }
--- SEP/src/model/RoomList.java
+++ SEP/src/model/RoomList.java
@@ -4,11 +4,167 @@ import java.util.ArrayList;
 
 public class RoomList {
 	
-	private ArrayList<Room> rooms = new ArrayList<Room>();
+	private static ArrayList<Room> roomsVaxjo = new ArrayList<Room>();
+	private static ArrayList<Room> roomsKalmar = new ArrayList<Room>();
 	
 	public void createRooms() {
-		for(int i = 0; i < 10; i++) {
-			
+		for(int i = 0; i < 15; i++) {
+			if (i <= 10) {
+				
+			Room roomVaxjo = singleNormalRoom("Växjo", i, 1, false, true);
+			Room roomKalmar = singleNormalRoom("Kalmar", i, 1, false, true);
+			if(i > 3) {
+				roomVaxjo.setAdjoinsRoom(true);
+				roomKalmar.setAdjoinsRoom(true);
+			}
+			roomsVaxjo.add(roomVaxjo);
+			roomsKalmar.add(roomKalmar);
+			}
+			else if (i > 10) {
+				Room roomVaxjo = singleQualityRoom("Växjö", i, 3, true, true);
+				Room roomKalmar = singleQualityRoom("Växjö", i, 3, true, true);
+				if(i > 10 && i < 13) {
+					roomVaxjo.setAdjoinsRoom(true);
+					roomKalmar.setAdjoinsRoom(true);
+				}
+				
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			if (i <=6) {
+				Room roomVaxjo = doubleNormalRoom("Växjö", i, 1, false, true);
+				Room roomKalmar = doubleNormalRoom("Kalmar", i, 1, false, true);
+				if (i < 3) {
+					roomVaxjo.setAdjoinsRoom(true);
+					roomKalmar.setAdjoinsRoom(true);
+				}
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+			else if(i > 6 && i <= 8) {
+				Room roomVaxjo = doubleQualityRoom("Växjö", i, 2, true, true);
+				Room roomKalmar = doubleQualityRoom("Kalmar", i, 2, true, true);
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+			else {
+				Room roomVaxjo = doubleQualityRoom("Växjö", i, 3, true, true);
+				Room roomKalmar = doubleQualityRoom("Kalmar", i, 3, true, true);
+				roomVaxjo.setAdjoinsRoom(true);
+				roomKalmar.setAdjoinsRoom(true);
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			if (i <= 3) {
+				Room roomVaxjo = tripleNormalRoom("Växjö", i, 1, false, true);
+				Room roomKalmar = tripleNormalRoom("Kalmar", i, 1, false, true);
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+			else if(i == 4) {
+				Room roomVaxjo = tripleQualityRoom("Växjö", i, 2, true, true);
+				Room roomKalmar = tripleQualityRoom("Kalmar", i, 2, true, true);
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
+			else {
+				Room roomVaxjo = tripleQualityRoom("Växjö", i, 3, true, true);
+				Room roomKalmar = tripleQualityRoom("Kalmar", i, 3, true, true);
+				roomsVaxjo.add(roomVaxjo);
+				roomsKalmar.add(roomKalmar);
+			}
 		}
 	}
+	
+	public Room singleNormalRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+	Room room = new Room();
+	
+	room.setHotelName(hotel);
+	room.setRoomNumber(roomNumber);
+	room.setQuality(quality);
+	room.setFloor(1);
+	room.setSmokingAllowed(smoking);
+	room.setAvailability(availability);
+	
+	
+	
+	return room;
+	}
+	
+	public Room singleQualityRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+		Room room = new Room();
+		
+		room.setHotelName(hotel);
+		room.setRoomNumber(roomNumber);
+		room.setQuality(quality);
+		room.setFloor(1);
+		room.setSmokingAllowed(smoking);
+		room.setAvailability(availability);
+		
+		
+		return room;
+	}
+	
+	public Room doubleNormalRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+		Room room = new Room();
+		
+		room.setHotelName(hotel);
+		room.setRoomNumber(roomNumber);
+		room.setQuality(quality);
+		room.setFloor(2);
+		room.setSmokingAllowed(smoking);
+		room.setAvailability(availability);
+		
+		
+		return room;
+	}
+	
+	public Room doubleQualityRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+		Room room = new Room();
+		
+		room.setHotelName(hotel);
+		room.setRoomNumber(roomNumber);
+		room.setQuality(quality);
+		room.setFloor(2);
+		room.setSmokingAllowed(smoking);
+		room.setAvailability(availability);
+		
+		
+		return room;
+	}
+	
+	public Room tripleNormalRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+		Room room = new Room();
+		
+		room.setHotelName(hotel);
+		room.setRoomNumber(roomNumber);
+		room.setQuality(quality);
+		room.setFloor(3);
+		room.setSmokingAllowed(smoking);
+		room.setAvailability(availability);
+		
+		
+		return room;
+	}
+	
+	public Room tripleQualityRoom(String hotel, int roomNumber, int quality, boolean smoking, boolean availability) {
+		Room room = new Room();
+		
+		room.setHotelName(hotel);
+		room.setRoomNumber(roomNumber);
+		room.setQuality(quality);
+		room.setFloor(3);
+		room.setSmokingAllowed(smoking);
+		room.setAvailability(availability);
+		
+		
+		return room;
+	}
+	
 }
