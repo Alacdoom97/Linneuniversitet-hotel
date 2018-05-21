b91182d19de8f32bcdf88254e121feb6c26fcbc4
Pranav <pranav.dilip.patel@gmail.com>
2018-05-21 17:35:29 +0200

    load guest into arraylist from database

--- SEP/src/main/Main.java
+++ SEP/src/main/Main.java
@@ -11,13 +11,13 @@ public class Main {
 	public GuestList gl = new GuestList();
 	public RoomList rl = new RoomList();
 	public Booking bl = new Booking();
-
 	ReservationController rc = new ReservationController(this);
 
 	ReservationController resControl = new ReservationController(this);
 	
 
 	public static void main(String[] args){
+	
 		Application.launch(MainWindow.class);
 		
 		
